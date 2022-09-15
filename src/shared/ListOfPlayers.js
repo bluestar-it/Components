e76ac66b2fce131a3d8b7550ee267630840d1a76
Thao My <mylttse161097@fpt.edu.vn>
2022-09-15 19:32:18 +0700

    Components Part 2

--- src/shared/ListOfPlayers.js
+++ src/shared/ListOfPlayers.js
@@ -1,5 +1,5 @@
-export const Players = [
-    {id:1, name: 'kante', club: 'Chelsea', img:'assets/images/kante.jpg'},
+export const PlayersList = [
+    {id:1, name: 'Kante', club: 'Chelsea', img:'assets/images/kante.jpg'},
     {id:2, name: 'Messi', club: 'PSG', img:'assets/images/messi.jpg'},
     {id:3, name: 'Neymar', club: 'PSG', img:'assets/images/neymar.jpg'},
     {id:4, name: 'Kane', club: 'Tottemham', img:'assets/images/kane.jpg'},
