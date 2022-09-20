29da8df5de4cd79081cfd9f45c5238420f7b657d
Thao My <mylttse161097@fpt.edu.vn>
2022-09-20 10:15:02 +0700

    fix-lab-1

--- src/components/FilmsPresentation.js
+++ src/components/FilmsPresentation.js
@@ -9,22 +9,17 @@ export default class FilmsPresentation extends Component {
           <div className="column">
             <div className="card">
               <div className="image">
-              <img src={film.image} />
-
+                <img src={film.image} />
               </div>
               <div className="content">
-
-              <h4>{film.title}</h4>
-              <p className="infor">{film.nation}</p>
-              <p className="infor">{film.year}</p>
-              <div className="buttons">
-
-              <button class="button fa fa-play">Watch Film</button>
-              <button class="button fa fa-plus">Add to playlist</button>
-              </div>
-              
+                <h4>{film.title}</h4>
+                <p className="infor">{film.nation}</p>
+                <p className="infor">{film.year}</p>
+                <div className="buttons">
+                  <button class="button fa fa-play"></button>
+                  <button class="button fa fa-plus "></button>
                 </div>
-              
+              </div>
             </div>
           </div>
         ))}
--- src/shared/ListOfFilms.js
+++ src/shared/ListOfFilms.js
@@ -1,11 +1,11 @@
 export const FilmsList =[
     {id: 1,image: 'assets/images/chamberofSecrets.jpg',title: "Harry Potter and the Chamber of Secrets",year: "2002",nation: "UK",}     ,
-    {id: 2,image: 'assets/images/phoenix.webp',title: "Harry Potter and the Order of the Phoenix",year: "2007",nation: "UK",}     ,
+    {id: 2,image: 'assets/images/phoenix.jpg',title: "Harry Potter and the Order of the Phoenix",year: "2007",nation: "UK",}     ,
     {id: 3,image: 'assets/images/BreakingDawn-p2.jpg',title: "The Twilight Saga: Breaking Dawn: Part 2",year: "2012",nation: "USA",}     ,
     {id: 4,image: 'assets/images/betterDays.jpg',title: "Better Days",year: "2019",nation: "China",}     ,
     {id: 5,image: 'assets/images/buy mom.jpg',title: "Goodbye Mother",year: "2019",nation: "VietNam",}     ,
     {id: 6,image: 'assets/images/13 Reasons Why.jpg',title: "13 Reasons Why",year: "2017 ",nation: "USA",}     ,
-    {id: 7,image: 'assets/images/no7.webp',title: "Miracle in Cell No.7",year: "2013",nation: "Korea",}     ,
+    {id: 7,image: 'assets/images/no7.jpg',title: "Miracle in Cell No.7",year: "2013",nation: "Korea",}     ,
     {id: 8,image: 'assets/images/naruto.jpg',title: "Naruto Shippuden",year: "2007",nation: "Japan",}     ,
     {id: 9,image: 'assets/images/nezha.jpg',title: "Ne Zha",year: "2019",nation: "China",}        
     ];
